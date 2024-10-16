cac9c120fcfd738503145be52a4584d4012f665b
Gustavo loyola <guh.loyola@gmail.com>
2024-10-16 08:18:19 -0300

    Arquivos deletados

--- calculo.py
+++
@@ -1,9 +0,0 @@
-# Solicita ao usuário para inserir dois números
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-# Calcula a soma
-soma = num1 + num2
-
-# Exibe o resultado
-print("A soma é:", soma)
